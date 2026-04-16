70891acfd5201eb403fd732eeba36045091f4b85
musfik070-gif <musfik070@gmail.com>
2026-04-17 01:25:01 +0600

    created navbar, need to import fonts

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,53 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import { FaHome } from "react-icons/fa";
+import { FaClock } from "react-icons/fa";
+import { FaChartBar } from "react-icons/fa";
 
 function Navbar() {
   return (
-    <div className="flex justify-between items-center px-10 py-4 bg-white shadow">
-      <h1 className="font-bold text-xl">KeenKeeper</h1>
+    <div className="flex justify-between items-center px-10 py-4 bg-[#f5f7f6] border-b">
+      {/* Logo */}
+      <h1 className="font-bold text-xl text-gray-800">
+        Keen<span className="text-green-700">Keeper</span>
+      </h1>
 
-      <div className="flex gap-6">
-        <Link to="/">Home</Link>
-        <Link to="/timeline">Timeline</Link>
-        <Link to="/stats">Stats</Link>
+      {/* Nav Links */}
+      <div className="flex items-center gap-6 text-gray-600">
+        <NavLink
+          to="/"
+          className={({ isActive }) =>
+            `flex items-center gap-2 px-4 py-2 rounded-lg ${
+              isActive ? "bg-green-700 text-white" : "hover:bg-gray-200"
+            }`
+          }
+        >
+          <FaHome />
+          Home
+        </NavLink>
+
+        <NavLink
+          to="/timeline"
+          className={({ isActive }) =>
+            `flex items-center gap-2 px-4 py-2 rounded-lg ${
+              isActive ? "bg-green-700 text-white" : "hover:bg-gray-200"
+            }`
+          }
+        >
+          <FaClock />
+          Timeline
+        </NavLink>
+
+        <NavLink
+          to="/stats"
+          className={({ isActive }) =>
+            `flex items-center gap-2 px-4 py-2 rounded-lg ${
+              isActive ? "bg-green-700 text-white" : "hover:bg-gray-200"
+            }`
+          }
+        >
+          <FaChartBar />
+          Stats
+        </NavLink>
       </div>
     </div>
   );
